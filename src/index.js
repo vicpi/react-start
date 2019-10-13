dfe5a0837a4fc0c28956f3ae0d223a2b1b31dccb
Viktor Pyskunov <vicpi89@gmail.com>
2019-10-13 19:54:57 +0300

    project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import FormContainer from "./components/FormContainer.jsx";
+
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
