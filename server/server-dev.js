3903f5056c5d33b4d3b36c8254f224b3dedf2011
Viktor Pyskunov <vicpi89@gmail.com>
2019-10-15 15:26:49 +0300

    webpack.dev.config.js

--- server/server-dev.js
+++ server/server-dev.js
@@ -9,7 +9,8 @@ const app = express()
 const compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+    index: false
 }))
 
 app.use(webpackHotMiddleware(compiler))
@@ -17,6 +18,17 @@ app.use(webpackHotMiddleware(compiler))
 app.get('/health', (req, res) =>
     res.json({success: true})
 );
+
+app.get('/', (req, res) => {
+    compiler.outputFileSystem.readFile(path.resolve(__dirname, '..', 'dist', 'index.html'), (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.set('content-type', 'text/html')
+    res.send(result)
+    res.end()
+    })
+});
 
 const PORT = process.env.PORT || 3000
 
