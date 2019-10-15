312505bd72900016d6371185570666a759a9235c
Viktor Pyskunov <vicpi89@gmail.com>
2019-10-15 14:36:06 +0300

    Configured webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,51 +1,142 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssNormalize = require('postcss-normalize');
+const safePostCssParser = require('postcss-safe-parser');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         index: ['./src/index.js']
     },
+    bail: true,
+    devtool: false,
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/static/',
-        filename: '[name].js'
+        filename: '[name].js',
+        chunkFilename: '[name].[contenthash:8].chunk.js'
     },
     mode: 'production',
-    resolve: {
-        alias: {
-            'react-dom': '@hot-loader/react-dom',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // This is only used in production mode
+            new TerserPlugin({
+                terserOptions: {
+                    parse: {
+                        ecma: 8,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        // Disabled because of an issue with Uglify breaking seemingly valid code:
+                        // https://github.com/facebook/create-react-app/issues/2376
+                        // Pending further investigation:
+                        // https://github.com/mishoo/UglifyJS2/issues/2011
+                        comparisons: false,
+                        // Disabled because of an issue with Terser breaking valid code:
+                        // https://github.com/facebook/create-react-app/issues/5250
+                        // Pending further investigation:
+                        // https://github.com/terser-js/terser/issues/120
+                        inline: 2,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true,
+                    },
+                },
+                parallel: true,
+                cache: true,
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions: {
+                    parser: safePostCssParser,
+                    map: false,
+                },
+            }),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            name: true,
         },
+    },
+    resolve: {
         modules: ['node_modules', 'src']
     },
     module: {
+        strictExportPresence: true,
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    cacheCompression: false,
+                    compact: true,
                 }
             },
             {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "html-loader"
+                        loader: 'html-loader'
                     }
                 ]
             },
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
-                ]
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                                require('postcss-preset-env')({
+                                    autoprefixer: {
+                                        flexbox: 'no-2009',
+                                    },
+                                    stage: 3,
+                                }),
+                                postcssNormalize(),
+                            ],
+                        },
+                    }
+                ],
+                sideEffects: true,
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [
+                                require('postcss-flexbugs-fixes'),
+                                require('postcss-preset-env')({
+                                    autoprefixer: {
+                                        flexbox: 'no-2009',
+                                    },
+                                    stage: 3,
+                                }),
+                                postcssNormalize(),
+                            ],
+                        },
+                    },
+                    'sass-loader'
+                ],
+                sideEffects: true,
             },
             {
                 test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
@@ -59,9 +150,28 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
+            template: './src/index.html',
+            filename: './index.html',
+            inject: true,
         }),
-        new webpack.HotModuleReplacementPlugin()
-    ]
+        new ModuleNotFoundPlugin('.'),
+        new MiniCssExtractPlugin({
+            filename: '/[name].[contenthash:8].css',
+            chunkFilename: '/[name].[contenthash:8].chunk.css',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            { from: 'public', to: '.' },
+        ]),
+    ],
+    node: {
+        module: 'empty',
+        dgram: 'empty',
+        dns: 'mock',
+        fs: 'empty',
+        http2: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        child_process: 'empty',
+    },
 }
