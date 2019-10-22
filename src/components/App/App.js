3e5dcbe08c793224c904e105f621e57538256525
Viktor Pyskunov <vicpi89@gmail.com>
2019-10-22 12:59:23 +0300

    Remove MainContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import { hot } from 'react-hot-loader/root'
 import React from 'react';
 import { Route } from 'react-router-dom';
-import MainContainer from 'containers/MainContainer/MainContainer';
+import Main from 'components/Main/Main';
 import Footer from 'components/Footer/Footer'
 import About from 'components/About/About';
 import './App.scss'
@@ -9,7 +9,7 @@ import './App.scss'
 const App = () => (
     <>
         <div className="App">
-            <Route exact path="/" component={MainContainer} />
+            <Route exact path="/" component={Main} />
             <Route exact path="/about/" component={About} />
         </div>
         <Footer />
